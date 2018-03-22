332957c11d91c6b29e26b302be06c15870837055
Matthieu Dorier <mdorier@anl.gov>
2018-03-22 15:59:58 +0100

    made __mid visible for outside, C-based providers

--- pymargo/__init__.py
+++ pymargo/__init__.py
@@ -14,13 +14,13 @@ class MargoAddress():
 		self.__need_del = need_del
 
 	def __del__(self):
-		_pymargo.addr_free(self.__mid, self.__hg_addr)
+		_pymargo.addr_free(self._mid, self.__hg_addr)
 
 	def __str__(self):
-		return _pymargo.addr2str(self.__mid, self.__hg_addr)
+		return _pymargo.addr2str(self._mid, self.__hg_addr)
 
 	def copy(self):
-		return MargoAddress(self.__mid, _pymargo.addr_dup(self.__mid, self.__hg_addr))
+		return MargoAddress(self._mid, _pymargo.addr_dup(self._mid, self.__hg_addr))
 
 	def get_hg_addr(self):
 		return self.__hg_addr
@@ -31,12 +31,12 @@ class MargoInstance():
 			mode=server, 
 			use_progress_thread=False,
 			rpc_location=in_caller_thread):
-		self.__mid = _pymargo.init(addr, mode, use_progress_thread, rpc_location)
+		self._mid = _pymargo.init(addr, mode, use_progress_thread, rpc_location)
 		self.__finalized = False
 
 	def __del__(self):
 		if not self.__finalized:
-			_pymargo.finalize(self.__mid);
+			_pymargo.finalize(self._mid);
 
 	def __enter__(self):
 		return self
@@ -45,43 +45,43 @@ class MargoInstance():
 		pass
 
 	def finalize(self):
-		_pymargo.finalize(self.__mid);
+		_pymargo.finalize(self._mid);
 		self.__finalized = True
 
 	def wait_for_finalize(self):
-		_pymargo.wait_for_finalize(self.__mid)
+		_pymargo.wait_for_finalize(self._mid)
 		self.__finalized = True
 
 	def on_finalize(self, callable_obj):
-		_pymargo.push_finalize_callback(self.__mid, callable_obj)
+		_pymargo.push_finalize_callback(self._mid, callable_obj)
 
 	def enable_remote_shutdown(self):
-		_pymargo.enable_remote_shutdown(self.__mid)
+		_pymargo.enable_remote_shutdown(self._mid)
 
 	def register(self, rpc_name, obj=None, method_name=None, mplex_id=0):
 		if (obj is None) and (method_name is None):
-			return _pymargo.register_on_client(self.__mid, rpc_name, mplex_id)
+			return _pymargo.register_on_client(self._mid, rpc_name, mplex_id)
 		elif (obj is not None) and (method_name is not None):
-			return _pymargo.register(self.__mid, rpc_name, mplex_id, obj, method_name)
+			return _pymargo.register(self._mid, rpc_name, mplex_id, obj, method_name)
 		else:
 			raise RuntimeError('MargoInstance.register: both method name and object instance should be provided')
 
 	def registered(self, rpc_name, mplex_id=None):
 		if mplex_id is None:
-			return _pymargo.registered(self.__mid, rpc_name) 
+			return _pymargo.registered(self._mid, rpc_name) 
 		else:
-			return _pymargo.registered_mplex(self.__mid, rpc_name, mplex_id)
+			return _pymargo.registered_mplex(self._mid, rpc_name, mplex_id)
 
 	def lookup(self, straddr):
-		hg_addr = _pymargo.lookup(self.__mid, straddr)
-		return MargoAddress(self.__mid, hg_addr)
+		hg_addr = _pymargo.lookup(self._mid, straddr)
+		return MargoAddress(self._mid, hg_addr)
 
 	def addr(self):
-		hg_addr = _pymargo.addr_self(self.__mid)
-		return MargoAddress(self.__mid, hg_addr)
+		hg_addr = _pymargo.addr_self(self._mid)
+		return MargoAddress(self._mid, hg_addr)
 
 	def create_handle(self, addr, rpc_id, mplex_id=0):
-		return _pymargo.create(self.__mid, addr.get_hg_addr(), rpc_id, mplex_id)
+		return _pymargo.create(self._mid, addr.get_hg_addr(), rpc_id, mplex_id)
 
 class Provider(object):
 

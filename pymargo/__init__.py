325107535b8d099a405b88f6410f80115542aa35
Matthieu Dorier <mdorier@anl.gov>
2019-02-25 05:45:06 -0600

    removed RPC locations

--- pymargo/__init__.py
+++ pymargo/__init__.py
@@ -5,9 +5,6 @@ from . import core
 client = core.client
 server = core.server
 
-in_caller_thread   = core.in_caller_thread
-in_progress_thread = core.in_progress_thread
-
 MargoAddress  = core.Address
 MargoHandle   = core.Handle
 MargoInstance = core.Engine

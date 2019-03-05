71ba486e1ba88c79757b61d01b3f1db093a0ebac
Matthieu Dorier <mdorier@anl.gov>
2019-03-05 08:47:59 -0600

    updated test for Python 3

--- test/client.py
+++ test/client.py
@@ -1,5 +1,5 @@
 import sys
-sys.path.append('build/lib.linux-x86_64-2.7')
+#sys.path.append('build/lib.linux-x86_64-2.7')
 import pymargo
 from pymargo.core import Engine
 
@@ -11,5 +11,5 @@ def call_rpc_on(mid, rpc_id, addr_str, provider_id, name):
 with Engine('tcp', mode=pymargo.client) as mid:
 	rpc_id = mid.register("say_hello")
 	ret = call_rpc_on(mid, rpc_id, sys.argv[1], int(sys.argv[2]), str(sys.argv[3]))
-	print str(ret)
+	print(str(ret))
 
--- test/server.py
+++ test/server.py
@@ -1,5 +1,5 @@
 import sys
-sys.path.append('build/lib.linux-x86_64-2.7')
+#sys.path.append('build/lib.linux-x86_64-2.7')
 from pymargo.core import Engine, Provider
 
 class HelloProvider(Provider):
@@ -9,17 +9,17 @@ class HelloProvider(Provider):
         self.register("say_hello", "hello")
 
     def hello(self, handle, name):
-        print "Hello from "+name
-        print "RPC id is "+str(handle.get_id())
+        print("Hello from "+name)
+        print("RPC id is "+str(handle.get_id()))
         handle.respond("Hi "+name+"!")
         self.get_margo_instance().finalize()
 
 def WhenFinalize():
-    print "Finalize was called"
+    print("Finalize was called")
 
 mid = Engine('tcp')
 provider_id = 42
-print "Server running at address " + str(mid.addr()) + " with provider_id " + str(provider_id)
+print("Server running at address " + str(mid.addr()) + " with provider_id " + str(provider_id))
 
 mid.on_finalize(WhenFinalize)
 provider = HelloProvider(mid, provider_id)
